f2d91f1928b295ecc7b5e80dee4a6dc362641b6b
MuhammadJakyyy7 <116238770+MuhammadJakyyy7@users.noreply.github.com>
2022-10-21 23:02:18 +0700

    Add files via upload

---
+++ Latihan 1 D2 For2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Loops{
+   class Program   {
+      static void Main(string[] args)  {
+        int  a, b, c;
+
+         Console.Write("masukan nilai awal  perulangan :");
+         b=Convert.ToInt32(Console.ReadLine());
+         Console.Write("masukan nilai akhir  perulangan :");
+         c=Convert.ToInt32(Console.ReadLine());
+
+         for (  a=b  ; a < c; a = a + 1)         {
+            Console.WriteLine("Perulangan for ke: {0}", a);
+         }
+         Console.ReadLine();
+      }
+   }
+} 
---
+++ Latihan 2 D2 While/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Loops {
+   class Program   {
+      static void Main(string[] args){
+         int a = 10;
+         while (a < 20)         {
+            Console.WriteLine("Perulangan While ke: {0}", a);
+            a++;
+         }
+         Console.ReadLine();
+      }
+   }
+}
